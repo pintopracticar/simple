f9fd14f2c61a5dfdcfa3639e8b9e26229c6510db
pintopracticar <pintopracticar@users.noreply.github.com>
2014-09-12 17:36:34 -0300

    Update angularstats.js

--- angularstats.js
+++ angularstats.js
@@ -1,6 +1,12 @@
 var moduloStatsShow = angular.module('statsShow', []);
+
 moduloStatsShow.filter('nombre', function(){
-    return function(nombre) {
+    return function(nombre){
         return 'Stats de ' + nombre;
     };
 });
+
+moduloStatsShow.controller('statsCtrl', function($scope){
+    $scope.comprador = frutadores[$scope.posicion];
+});
+
